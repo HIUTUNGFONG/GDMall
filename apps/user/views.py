d2b93f7bdd32f608e41767688a4ae9923555db60
HIUTUNGFONG <460924276@qq.com>
2019-05-05 16:08:10 +0800

    添加redis缓存

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,11 +1,10 @@
-
+import os
 
 import requests
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from django_redis import get_redis_connection
-from common.public_function import PublicFunction
 
 class TokenView(APIView):
     '''
@@ -14,7 +13,7 @@ class TokenView(APIView):
 
     def get(self,request, code):
         # 生成一个随机3rd_session
-        # session_key = PublicFunction.get3rd_session()
+        session_key = os.popen('head -n 80 /dev/urandom | tr -dc A-Za-z0-9 | head -c 64').read()
         app_id = 'wxd2352a7b57c51606'
         app_secret = 'ef6117603e0cd75c1b280f35ed288994'
         url = 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'%(app_id,app_secret,code)
@@ -23,7 +22,7 @@ class TokenView(APIView):
         print(result)
 
         conn = get_redis_connection('default')
-        conn.hset('123456153a4s86d5a45sd',data['openid'],'test')
+        conn.hset(session_key,data['openid'] + '&&&&' + data['session_key'])
 
 
         return Response(data)
