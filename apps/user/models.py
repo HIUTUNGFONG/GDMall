301f50dfad2f16aff3aa40102e927bdcaeb55e90
HIUTUNGFONG <460924276@qq.com>
2019-05-09 15:33:32 +0800

    添加User表模型

--- apps/user/models.py
+++ apps/user/models.py
@@ -5,32 +5,34 @@ from django.db import models
 from common.base_model import BaseModel
 
 
-
-
 class User(BaseModel, AbstractUser):
     '''
     用户信息模型
     '''
     openid = models.CharField(max_length=50, verbose_name='用户openid')
-    nick_name = models.CharField(max_length=50,blank=True, verbose_name='昵称')
-    head_portrait = models.URLField(blank=True,verbose_name='用户头像')
-    phone = models.IntegerField(max_length=11,null=True, verbose_name='手机号码')
-    integral = models.IntegerField(max_length=20,default=0, verbose_name='会员积分')
+    nick_name = models.CharField(max_length=50, blank=True, verbose_name='昵称')
+    head_portrait = models.CharField(max_length=255, blank=True, verbose_name='用户头像')
+    birthday = models.CharField(max_length=50, blank=True, verbose_name='出生日期')
+    height = models.CharField(max_length=30, blank=True, verbose_name='身高')
+    weight = models.CharField(max_length=30, blank=True, verbose_name='体重')
+    phone = models.CharField(max_length=11, null=True, verbose_name='手机号码')
+    integral = models.IntegerField(max_length=20, default=0, verbose_name='会员积分')
 
     class Meta:
         db_table = 'gd_user'
         verbose_name = '用户信息'
         verbose_name_plural = verbose_name
 
+
 class Address(BaseModel):
     '''
     用户收货地址
     '''
-    openid = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='用户openid')
-    addressee = models.CharField(max_length=30,verbose_name='收件人')
-    phone = models.IntegerField(max_length=11,null=True,verbose_name='联系人电话')
-    shipping_address = models.CharField(max_length=255,verbose_name='收件地址')
-    is_default = models.BooleanField(default=0,verbose_name='是否默认')
+    openid = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='用户openid')
+    addressee = models.CharField(max_length=30, verbose_name='收件人')
+    phone = models.CharField(max_length=11, null=True, verbose_name='联系人电话')
+    shipping_address = models.CharField(max_length=255, verbose_name='收件地址')
+    is_default = models.BooleanField(default=0, verbose_name='是否默认')
 
     class Meta:
         db_table = 'gd_address'
@@ -42,8 +44,8 @@ class VipLevel(BaseModel):
     '''
     会员等级
     '''
-    level = models.CharField(max_length=20,verbose_name='等级名称')
-    minmum_integral = models.IntegerField(max_length=10,verbose_name='积分要求值')
+    level = models.CharField(max_length=20, verbose_name='等级名称')
+    minmum_integral = models.IntegerField(verbose_name='积分要求值')
 
     class Meta:
         db_table = 'gd_vip_level'
