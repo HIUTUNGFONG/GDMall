e7e719bfcd67dec8783ab75689144ce3fcd05dad
HIUTUNGFONG <460924276@qq.com>
2019-05-14 18:02:18 +0800

    修改uwsgi配置文件

--- apps/user/views.py
+++ apps/user/views.py
@@ -47,11 +47,11 @@ class CreateUserView(APIView):
         conn = get_redis_connection('UserToken')
         result = conn.get(data['token'])
         # 获取openid
-        openid = result.split('$$$$')[0]
+        openid = str(result).split('$$$$')[0]
         print(openid)
         # openid = data['openid']
         # 获取session
-        session = result.split('$$$$')[1]
+        session = str(result).split('$$$$')[1]
         print(session)
         # session = data['session']
         # 判断数据库是否存在该openid
