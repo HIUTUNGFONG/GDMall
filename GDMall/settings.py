652608c19e7184852ba07d8969284da5d98f07ab
HIUTUNGFONG <460924276@qq.com>
2019-05-14 17:32:54 +0800

    修改uwsgi配置文件

--- GDMall/settings.py
+++ GDMall/settings.py
@@ -163,7 +163,8 @@ CACHES = {
     },
     "UserToken": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://:123456@127.0.0.1:6379/1",
+        "LOCATION": "redis://:123456@192.168.0.163:6379/1",
+        # "LOCATION": "redis://:123456@127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
