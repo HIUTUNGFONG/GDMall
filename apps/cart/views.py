06ab5962bc1e0b4b2ffbeed23aee3696fdc02099
HIUTUNGFONG <460924276@qq.com>
2019-05-25 21:18:37 +0800

    新增购物车添加商品api

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -52,7 +52,7 @@ class CartAddView(APIView):
         result = str(conn_ut.get(token))
         openid = result.split('$$$$')[0]
         user = WxUser.objects.filter(openid=openid)
-        user_id = user.id
+        user_id = user['id']
         conn = get_redis_connection('Cart')
         cart_key = 'cart_%d' % user_id
         # 先尝试获取commodity_id的值 hget cart_key 属性
