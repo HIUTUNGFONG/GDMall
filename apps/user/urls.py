8ca38c0428379eef9bba9a2b15170987510249f7
HIUTUNGFONG <460924276@qq.com>
2019-06-05 00:26:40 +0800

    添加收货地址api

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     re_path('getToken/(?P<code>.*)', TokenView.as_view(), name='getToken'),  # GET获取微信openid
     re_path('getRedisToken/(?P<token>.*)', RedisTokenView.as_view(), name='getRedisToken'),  # GET查询Redis的token
     path('createUser', CreateUserView.as_view(), name='createUser'),  # POST添加用户
+    re_path('addr/(?P<token>.*)', AddressView.as_view(), name='getAdddressList'),  # GET获取用户地址
+    path('addr', AddressView.as_view(), name='createAddress'),  # POST添加用户地址
 ]
