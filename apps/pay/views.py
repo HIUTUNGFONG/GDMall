c0fece4f7dbc5e99dffc64be388539f93c5b37b6
HIUTUNGFONG <460924276@qq.com>
2019-06-13 00:10:58 +0800

    添加微信支付api接口

--- apps/pay/views.py
+++ apps/pay/views.py
@@ -36,7 +36,7 @@ class WxPayView(APIView):
             'nonce_str': str32,
             'body': 'test支付',
             'out_trade_no': orderNum,
-            'total_fee': '10',
+            'total_fee': 1,
             'spbill_create_ip': '47.112.147.15',
             'notify_url': 'http://www.grotesquery.cn/api/pay/get',
             'trade_type': 'JSAPI'
