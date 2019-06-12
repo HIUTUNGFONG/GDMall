3532aaaf8fbbcc63d8fd27c0dc30e75063a040f4
HIUTUNGFONG <460924276@qq.com>
2019-06-12 23:03:16 +0800

    添加微信支付api接口

--- apps/pay/views.py
+++ apps/pay/views.py
@@ -10,15 +10,16 @@ from common.public_function import *
 pay_url = 'https://api.mch.weixin.qq.com/pay/unifiedorder'
 
 wxinfo = {
-    'APPID':'wxc999b4ac2adc328e',
-    'SECRET':'8b4f824b7d81a2a2b091eca8c9eeb2ba',
-    'MCHID':'1535717001',
-    'MCHKEY':'guanxinguanxinguanxinguanxin3344'
+    'APPID': 'wxc999b4ac2adc328e',
+    'SECRET': '8b4f824b7d81a2a2b091eca8c9eeb2ba',
+    'MCHID': '1535717001',
+    'MCHKEY': 'guanxinguanxinguanxinguanxin3344'
 }
+
 
 class WxPayView(APIView):
 
-    def post(self,request):
+    def post(self, request):
         # 接收数据
         data = json.loads(request.body)
         token = data['token']
@@ -29,32 +30,33 @@ class WxPayView(APIView):
         str32 = PublicFunction().randomStr()
         orderNum = PublicFunction.orderNum()
         params = {
-            'openid':openid,
-            'appid':wxinfo['APPID'],
-            'mch_id':wxinfo['MCHID'],
-            'nonce_str':str32,
-            'body':'test支付',
-            'out_trade_no':orderNum,
+            'openid': openid,
+            'appid': wxinfo['APPID'],
+            'mch_id': wxinfo['MCHID'],
+            'nonce_str': str32,
+            'body': 'test支付',
+            'out_trade_no': orderNum,
             'total_fee': '1000',
-            'spbill_create_ip':'47.112.147.15',
-            'notify_url':'http://www.grotesquery.cn/api/pay/get',
-            'trade_type':'JSAPI'
+            'spbill_create_ip': '47.112.147.15',
+            'notify_url': 'http://www.grotesquery.cn/api/pay/get',
+            'trade_type': 'JSAPI'
         }
-        sign = PublicFunction.wx_sign(params,wxinfo['MCHKEY'])
-        params['sign']=sign
-        xmlmsg = PublicFunction.send_xml_request(pay_url,params)
-        if xmlmsg['xml']['return_code']=='SUCCESS':
-            prepay_id=xmlmsg['xml']['prepay_id']
-            timeStamp=str(int(time.time()))
-            pay_data = {
-                'appId':wxinfo['APPID'],
-                'nonceStr':str32,
-                'package':'prepay_id'+prepay_id,
-                'signType':'MD5',
-                'timeStamp':timeStamp
-            }
-            # 再次签名
-            paySign=PublicFunction.wx_sign(pay_data)
-            pay_data['paySign'] = paySign
-
+        sign = PublicFunction.wx_sign(params, wxinfo['MCHKEY'])
+        params['sign'] = sign
+        print(params)
+        # xmlmsg = PublicFunction.send_xml_request(pay_url, params)
+        # if xmlmsg['xml']['return_code'] == 'SUCCESS':
+        #     prepay_id = xmlmsg['xml']['prepay_id']
+        #     timeStamp = str(int(time.time()))
+        #     pay_data = {
+        #         'appId': wxinfo['APPID'],
+        #         'nonceStr': str32,
+        #         'package': 'prepay_id' + prepay_id,
+        #         'signType': 'MD5',
+        #         'timeStamp': timeStamp
+        #     }
+        #     # 再次签名
+        #     paySign = PublicFunction.wx_sign(pay_data)
+        #     pay_data['paySign'] = paySign
+        pay_data = {}
         return Response(pay_data)
