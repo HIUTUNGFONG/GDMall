e7e80c99da35abe9c4649b4ff76962228ec75eb9
HIUTUNGFONG <460924276@qq.com>
2019-06-18 23:43:23 +0800

    修改微信支付订单号

--- apps/pay/views.py
+++ apps/pay/views.py
@@ -114,7 +114,7 @@ class PayView(APIView):
             print('transaction_id:'+transaction_id)
             # 获取自己的数据
             try:
-                order_info = OrderInfo.objects.get(order_id=out_trade_no)
+                order_info = OrderInfo.objects.filter(order_id=out_trade_no)
                 print(order_info)
                 # 订单金额
                 total_price = str(int(order_info.total_price*100))
