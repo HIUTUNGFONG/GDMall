83a8a435b5e14f089c4eca0d411070771fea1a0f
HIUTUNGFONG <460924276@qq.com>
2019-06-20 00:29:53 +0800

    添加根据order_info_id获取订单详情

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('order/create', CreateOrderView.as_view(), name='CreateOrderView'),  # POST 创建订单
     re_path('order/get/(?P<token>.*)', OrderView.as_view(), name='OrderView'),  # GET 获取订单
     path('order/get', OrderView.as_view(), name='OrderView'),  # POST 获取订单清单
+    path('order/getById', OrderByOrderIdView.as_view(), name='OrderByOrderIdView'),  # POST 获取订单清单
     path('order/del', DeleteOrderView.as_view(), name='DeleteOrderView'),  # POST 删除订单
     path('order/clean', CleanInvalidOrderView.as_view(), name='CleanInvalidOrderView'),  # POST 清除过期未支付订单
 
