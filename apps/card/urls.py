c226afd0cba497dd730f75705cbdebc00700825c
HIUTUNGFONG <460924276@qq.com>
2019-06-24 17:05:18 +0800

    添加获取wx user info的api

--- apps/card/urls.py
+++ apps/card/urls.py
@@ -1,11 +1,12 @@
 from django.urls import re_path,path
 
+from apps.card.views import CardView
 from apps.cart.views import *
 from apps.user.views import *
 
 app_name = 'cart'
 
 urlpatterns = [
-    path('card/add', CartAddView.as_view(), name='add'),
+    re_path('card/get/(?P<token>.*)', CardView.as_view(), name='CardView'),
 
 ]
--- apps/card/views.py
+++ apps/card/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from requests import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from apps.card.models import Card
+from apps.user.models import WxUser
+from common.public_function import PublicFunction
+
+
+class CardView(APIView):
+
+    '''
+    获取优惠券列表
+    '''
+    def get(self,request,token):
+
+        def get(self, request, token):
+
+            # 参数校验
+            if not all([token]):
+                return Response({'msg': '数据不完整'})
+
+            # 获取用户open_id
+            open_id = PublicFunction().getOpenIdByToken(token)
+
+            # 校验用户
+            if open_id:
+                try:
+                    wx_user = WxUser.objects.get(open_id=open_id)
+                except:
+                    return Response({'msg': '用户不存在'})
+                try:
+                    card = Card.objects.filter(is_delete=0).values()
+                except:
+                    return Response({'msg': '暂无优惠券活动'})
+
+            return Response({'card_list':card})
