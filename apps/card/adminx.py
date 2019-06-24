00112c3839b97c9c90236f20332a02e4cbae4503
HIUTUNGFONG <460924276@qq.com>
2019-06-24 15:47:40 +0800

    添加获取wx user info的api

--- apps/card/adminx.py
+++ apps/card/adminx.py
@@ -3,7 +3,10 @@ import xadmin
 
 class CardAdmin(object):
     pass
+class UserCardAdmin(object):
+    pass
 
 
 
 xadmin.site.register(Card, CardAdmin)
+xadmin.site.register(UserCard, UserCardAdmin)
