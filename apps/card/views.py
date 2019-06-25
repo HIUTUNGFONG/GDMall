f237f5613e8b766a1d74834baa6e54a4565a0bc2
HIUTUNGFONG <460924276@qq.com>
2019-06-25 17:19:04 +0800

    新增获取用户卡券列表api/添加优惠券api

--- apps/card/views.py
+++ apps/card/views.py
@@ -96,8 +96,8 @@ class AddCardView(APIView):
 
                 card = Card.objects.get(id=card_id)
                 user_card = UserCard.objects.filter(wx_user=wx_user)
-                print(user_card)
-                if (user_card.values() == None):
+                print(user_card.values())
+                if (user_card.values() == []):
                     print('t')
                     UserCard.objects.create(card_id=card_id, wx_user=wx_user, card_token=str32).save()
                 else:
