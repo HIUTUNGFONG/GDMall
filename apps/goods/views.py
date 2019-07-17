7ec4e9612b53dbf16fc5e65b6d78bc744621fd84
HIUTUNGFONG <460924276@qq.com>
2019-07-17 22:45:02 +0800

    修改goods_image返回

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -70,11 +70,15 @@ class GoodsListView(APIView):
         data_list = []
 
         for g in goods:
-            goods_image = GoodsImageSerializers(GoodsImage.objects.filter(goods_id=g['id'], is_delete=0), many=True)
+            # goods_image = GoodsImageSerializers(GoodsImage.objects.filter(goods_id=g['id'], is_delete=0), many=True)
+            goods_image_0 = GoodsImageSerializers(
+                GoodsImage.objects.filter(goods_id=g['id'], show_region=0, is_delete=0), many=True)
+            goods_image_1 = GoodsImageSerializers(
+                GoodsImage.objects.filter(goods_id=g['id'], show_region=1, is_delete=0), many=True)
             commodity = CommoditySerializers(Commodity.objects.filter(goods_id=g['id'], is_delete=0), many=True)
             sort = Sort.objects.filter(id=g['sort_id'], is_delete=0).values()
             classify = Classify.objects.filter(id=g['classify_id'], is_delete=0).values()
-            data_list.append([{'goods': g, 'goods_image': goods_image.data, 'commodity': commodity.data, 'sort': sort,
+            data_list.append([{'goods': g, 'goods_image_0': goods_image_0.data, 'goods_image_1': goods_image_1.data, 'commodity': commodity.data, 'sort': sort,
                                'classify': classify}])
 
         data = {'data': data_list}
