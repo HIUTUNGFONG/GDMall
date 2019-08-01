eefc09ef1d8e35afcd23230762f8d10407bbb97f
HIUTUNGFONG <460924276@qq.com>
2019-08-01 11:25:29 +0800

    保留两位小数

--- apps/card/apps.py
+++ apps/card/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class CardConfig(AppConfig):
     name = 'apps.card'
+    verbose_name = '优惠券信息'
--- apps/order/apps.py
+++ apps/order/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class OrderConfig(AppConfig):
     name = 'apps.order'
+    verbose_name = '订单信息'
